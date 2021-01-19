76810fb80975a0d9c4201dc831b8e3710a430c85
Loan Cardoso <jow13_122@yahoo.com>
2021-01-19 12:18:29 -0300

    Setting up and studying routes possibilities

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,42 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    console.log('Listage de usuários');
+app.use(express.json());
 
-    res.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Daniel'
-    ]);
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel'
+];
+
+app.get('/users/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    const user = users[id];
+
+    return res.json(user);
+});
+
+app.get('/users', (req, res) => {
+    const search = String(req.query.search);
+
+    const filteredUsers = search ? users.filter( user => user.includes(search) ) : users;
+
+    return res.json(filteredUsers);
+});
+
+app.post('/users', (req, res) => {
+    const data = req.body;
+
+    console.log(data);
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return res.json(user);
 });
 
 app.listen(3333);
