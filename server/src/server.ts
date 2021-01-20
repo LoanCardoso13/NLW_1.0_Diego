b6470ed7b93b8d6a100997f9dcd51e5cf2b077af
Loan Cardoso <jow13_122@yahoo.com>
2021-01-20 12:04:44 -0300

    Finished routes and beginning database with knex and sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,43 +1,13 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-const users = [
-    'Diego',
-    'Cleiton',
-    'Robson',
-    'Daniel'
-];
-
-app.get('/users/:id', (req, res) => {
-    const id = Number(req.params.id);
-
-    const user = users[id];
-
-    return res.json(user);
-});
-
-app.get('/users', (req, res) => {
-    const search = String(req.query.search);
-
-    const filteredUsers = search ? users.filter( user => user.includes(search) ) : users;
-
-    return res.json(filteredUsers);
-});
-
-app.post('/users', (req, res) => {
-    const data = req.body;
-
-    console.log(data);
-
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-    return res.json(user);
-});
+// Knex makes equivalent:
+// SELECT * FROM users WHERE name = 'Jow'
+// knex('users).where('name', 'Jow').select('*')
 
 app.listen(3333);
